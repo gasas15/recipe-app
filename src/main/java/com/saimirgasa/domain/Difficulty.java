152984724ed59a2f829485bc0da1df30caa81503
Saimir Gasa <saimir.gasa@gmail.com>
2020-12-01 15:57:31 +0100

    Added Difficulty Enum.

---
+++ src/main/java/com/saimirgasa/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.saimirgasa.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/saimirgasa/domain/Recipe.java
+++ src/main/java/com/saimirgasa/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +28,8 @@ public class Recipe {
     private String url;
     private String directions;
 
-//    private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -115,5 +118,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
